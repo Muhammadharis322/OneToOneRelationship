477d04e13ab3e1dc6a9028b12b0669893e21033b
Muhammadharis322 <harisazam322@gmail.com>
2021-09-23 01:23:48 +0500

    Add project files.

---
+++ OneToOneRelationship/Models/SchoolContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace OneToOneRelationship.Models
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options)
+         : base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-LMFF1UJ;Initial Catalog=OneToOneRelationship;Integrated Security=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>()
+                .HasOne<StudentAddress>(s => s.Address)
+                .WithOne(ad => ad.Student)
+                .HasForeignKey<StudentAddress>(ad => ad.AddressOfStudentId);
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<StudentAddress> StudentAddresses { get; set; }
+    
+        
+    }
+}
